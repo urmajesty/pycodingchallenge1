80b1afa3167d879d1bfa047b02cab05a6e3d2391
Chy <chyrellelewis@gmail.com>
2020-01-28 16:29:28 +0000

    separated into folders

---
+++ pfa01282020.py
@@ -0,0 +1,19 @@
+# coding challenge! Post your results Tuesday, 1/28/2020!
+# Given an int n, return the absolute difference between n and 21, except return double the absolute difference if n is over 21.
+# diff21(19) → 2
+# diff21(10) → 11
+# diff21(21) → 0
+
+#********************************************
+#CODING CHALLENGE SOLUTION... UNCOMMENT TO RUN
+# def diff21(n):
+#     if n < 22:
+#         return 21 - n
+#     else:
+#         return (n - 21) * 2 
+
+# print(diff21(-1))
+# print(diff21(-10))
+# print(diff21(21))
+#********************************************
+#CODING CHALLENGE SOLUTION... UNCOMMENT TO RUN 
