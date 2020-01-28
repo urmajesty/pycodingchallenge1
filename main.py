e287c9dac565b6d9bec1ab3e097c3d71b3efbac8
Chy <chyrellelewis@gmail.com>
2020-01-28 15:55:33 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,21 @@
+# Write a function that follows the following logic:
+# We have two monkeys, a and b, and the parameters a_smile and b_smile indicate if each is smiling. We are in trouble if they are both smiling or if neither of them is smiling. Return True if we are in trouble.
+
+# monkey_trouble(True, True) → True
+# monkey_trouble(False, False) → True
+# monkey_trouble(True, False) → False
+
+# def monkey_trouble(a_smile, b_smile):
+
+# Write out the monkey_trouble function share your solution after Monday 12 Noon EST.
+
+
+a_smile = False
+b_smile = True
+
+def monkey_trouble(a_smile, b_smile):
+  if a_smile == b_smile:
+    return(True)
+  else:
+    return(False)
+print(monkey_trouble(a_smile, b_smile))
